ddf1b8a33b88f35f827ecd3edafe15c97a371962
fitgeek96 <fitgeek96@gmail.com>
2021-06-21 10:39:39 +0300

    Putting all together

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function createStore() {
+// Library Code 
+
+function createStore(reducer) {
     // The Store should have four parts 
     // 1. The state 
     // 2. Get the state 
@@ -17,19 +19,60 @@ function createStore() {
         }
     }
 
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach((listener) => listener());
+    }
+
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 }
 
-const store = createStore();
+
+// App code 
+
+function todos(state = [], action) {
+
+    switch (action.type) {
+        case 'ADD_TODO':
+            return state.concat([action.todo]);
+        case 'REMOVE_TODO':
+            return state.filter((todo) => todo.id !== action.id);
+        case 'TOGGLE_TODO':
+            return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, {
+                complete: !todo.complete
+            }));
+        default:
+            return state;
+    }
+
+}
+
+function goals(state = [], action) {
+    switch (action.type) {
+        case 'ADD_GOAL':
+            return state.concat([action.goal]);
+        case 'REMOVE_GOAL':
+            return state.filter((goal) => goal.id !== action.id);
+        default:
+            return state;
+    }
+}
+
+const store = createStore(todos);
+
 store.subscribe(() => {
-    console.log('The new State is : ' , store.getState());
-});
-const unsubscribe = store.subscribe(() => {
-    console.log('The store has changed.');
+    console.log('The new State is : ', store.getState());
 });
 
-unsubscribe();
-
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false
+    }
+})
